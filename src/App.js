f591475d5e159c15bc3995c7e4f9d60b037a237e
gitdegsew <abebawdegsew@gmail.com>
2022-12-08 11:59:08 -0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,67 @@
+import { useEffect, useState } from "react";
+import useMediaQuery from "./hooks/useMediaQuery";
+import NavBar from "./scenes/NavBar";
+import DotGroup from "./scenes/DotGroup";
+import Landing from "./scenes/Landing";
+import MySkills from "./scenes/MySkills";
+import Projects from "./scenes/Projects";
+import Testimonials from "./scenes/Testimonials";
+import LineGradient from "./components/LineGradient";
+import Contact from "./scenes/Contact";
+import Footer from "./scenes/Footer";
+
+function App() {
+  const [selectedPage, setSelectedPage] = useState("home");
+  const isAboveMediumScreen = useMediaQuery("(min-width:1060px)");
+  const [isTopPage, setIsTopPage] = useState(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY === 0) setIsTopPage(true);
+      if (window.scrollY !== 0) setIsTopPage(false);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  return (
+    <div className="app bg-deep-blue ">
+      <NavBar
+        isTopPage={isTopPage}
+        selectedPage={selectedPage}
+        setSelectedPage={setSelectedPage}
+      />
+      <div className="w-5/6 mx-auto md:h-full">
+        {isAboveMediumScreen && (
+          <DotGroup
+            selectedPage={selectedPage}
+            setSelectedPage={setSelectedPage}
+          />
+        )}
+        <Landing setSelectedPage={setSelectedPage} />
+      </div>
+      <LineGradient />
+      <div className="w-5/6 mx-auto md:h-full">
+        <MySkills />
+      </div>
+      <LineGradient />
+      <div className="w-5/6 mx-auto ">
+        <Projects />
+      </div>
+      <LineGradient />
+      <div className="w-5/6 mx-auto h-full">
+        <Testimonials />
+      </div>
+      <LineGradient />
+      <div className="w-5/6 mx-auto h-full">
+        <Contact />
+      </div>
+      <LineGradient />
+      <div className=" mx-auto h-full w-full">
+        <Footer />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/scenes/NavBar.jsx
@@ -0,0 +1,109 @@
+import { useState } from "react";
+import AnchorLink from "react-anchor-link-smooth-scroll";
+import useMediaQuery from "../hooks/useMediaQuery";
+// import image from "../assets/menu-icon.svg";
+
+const Link = ({ page, selectedPage, setSelectedPage }) => {
+  const lowerCasePage = page.toLowerCase();
+  return (
+    <AnchorLink
+      className={`${selectedPage === lowerCasePage ? "text-yellow" : ""}
+        hover:text-yellow transition duration-500
+      `}
+      href={`#${lowerCasePage}`}
+      onClick={() => setSelectedPage(lowerCasePage)}
+    >
+      {page}
+    </AnchorLink>
+  );
+};
+
+const NavBar = ({ isTopPage, selectedPage, setSelectedPage }) => {
+  const [isMenuToggled, setIsMenuToggled] = useState(false);
+  const isAboveMediumScreen = useMediaQuery("(min-width:768px)");
+  const navBackground = isTopPage ? "" : "bg-red";
+  return (
+    <nav id="nav" className={`${navBackground} z-40 w-full fixed top-0 py-6`}>
+      <div className="flex items-center justify-between mx-auto w-5/6">
+        <h4 className="font-playfair text-3xl font-bold">JE</h4>
+        {isAboveMediumScreen ? (
+          <div className="flex justify-between gap-16 font-opensans text-sm font-semibold">
+            <Link
+              page="Home"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+            <Link
+              page="Skills"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+            <Link
+              page="Projects"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+            <Link
+              page="Testimonials"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+            <Link
+              page="Contact"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+          </div>
+        ) : (
+          <button
+            className="rounded-full bg-red p2"
+            onClick={() => setIsMenuToggled(!isMenuToggled)}
+          >
+            <img src="assets/menu-icon.svg" alt="menu-icon" />
+          </button>
+        )}
+        {!isAboveMediumScreen && isMenuToggled && (
+          <div className="fixed right-0 bottom-0 h-full bg-blue w-[200px]">
+            <div className="flex justify-end p-12">
+              <button
+                className="rounded-full bg-red p2"
+                onClick={() => setIsMenuToggled(!isMenuToggled)}
+              >
+                <img src="assets/close-icon.svg" alt="close-icon" />
+              </button>
+            </div>
+            <div className=" flex  flex-col  gap-6  ml-[33%] text-2xl text-deep-blue">
+              <Link
+                page="Home"
+                selectedPage={selectedPage}
+                setSelectedPage={setSelectedPage}
+              />
+              <Link
+                page="Skills"
+                selectedPage={selectedPage}
+                setSelectedPage={setSelectedPage}
+              />
+              <Link
+                page="Projects"
+                selectedPage={selectedPage}
+                setSelectedPage={setSelectedPage}
+              />
+              <Link
+                page="Testimonials"
+                selectedPage={selectedPage}
+                setSelectedPage={setSelectedPage}
+              />
+              <Link
+                page="Contact"
+                selectedPage={selectedPage}
+                setSelectedPage={setSelectedPage}
+              />
+            </div>
+          </div>
+        )}
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
